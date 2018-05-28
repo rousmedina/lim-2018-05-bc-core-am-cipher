06581e9f87d11bdcff1bcbeceb1264434a4d2a55
rousmedina <rousmedina21@gmail.com>
2018-05-28 15:53:16 -0500

    subiendo botones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.getElementById("btn1").addEventListener("click", function(){
-    document.getElementById("text1").innerHTML = "hola mundo";
+const textencode = document.getElementById("Text-encode");
+const textdecode = document.getElementById("text-decode");
+
+const btncipher = document.getElementById("btn-cipher");
+const btndecipher = document.getElementById("btn-decipher");
+
+btn-cipher.addEventListener( "click" ,() => {
 });
+btn-decipher.addEventListener( "click" ,() => {
+});
+
+
