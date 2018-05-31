eafb00b0135ead6ef63464423f555fb064333dee
rousmedina <rousmedina21@gmail.com>
2018-05-31 10:23:12 -0500

    subiendo encodey decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,31 @@
 window.cipher = {
-
-    encode:(offset,string) => {
-        let complet = '';
-        for (i = 0; i < string.length; i++) {
-          let toAscii = string.charCodeAt(i);
-          if (toAscii >= 65 && toAscii <= 90) {
-            complet += String.fromCharCode((toAscii - 65 + parseInt(offset)) % 26 + 65); // Mayus
-
-          } 
-          else if (toAscii >= 97 && toAscii <= 122) {
-            complet += String.fromCharCode((toAscii - 97 + parseInt(offset)) % 26 + 97); // Minus
-        }
-    else if (toAscii == 32){
+  encode:(offset,string) => {
+    let complet = '';
+    for (i = 0; i < string.length; i++) {
+      let toAscii = string.charCodeAt(i);
+      if (toAscii >= 65 && toAscii <= 90) {
+        complet += String.fromCharCode((toAscii - 65 + parseInt(offset)) % 26 + 65); // Mayus
+      } else if (toAscii >= 97 && toAscii <= 122) {
+        complet += String.fromCharCode((toAscii - 97 + parseInt(offset)) % 26 + 97); // Minus
+      } else if (toAscii == 32) {
         complet += "";
-     }
-   }        
- return complet;    
-     }
-      
-      decode:(offset,string) => {
-        let complet = '';
-        for (i = 0; i < inpuTxt.length; i++) {
-          let toAscii = inpuTxt.charCodeAt(i);
-          if (toAscii >= 65 && toAscii <= 90)
-            newPhrase += String.fromCharCode((toAscii - 65 - offset + 26 * 2) % 26 + 65);
-          else if (toAscii >= 97 && toAscii <= 122)
-            newPhrase += String.fromCharCode((toAscii - 97 - offset + 26 * 2) % 26 + 97);
-          else
-     
       }
-    }
+    }        
+    return complet;    
+  },
+
+  decode:(offset,string) => {
+    let complet = '';
+    for (i = 0; i < string.length; i++) {
+      let toAscii = string.charCodeAt(i);
+      if (toAscii >= 65 && toAscii <= 90) {
+        complet += String.fromCharCode((toAscii + 65 - parseInt(offset)) % 26 + 65); // Mayus
+      } else if (toAscii >= 97 && toAscii <= 122) {
+        complet += String.fromCharCode((toAscii + 97 - parseInt(offset)-12) % 26 + 97); // Minus
+      } else if (toAscii == 32) {
+        complet += "";
+      }
+    }        
+    return complet;    
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-const cipherBtn = document.getElementById('cipher'),
+const offset = document.getElementById('key');
+const string = document.getElementById("inputText")
 
-deCipherBtn = document.getElementById('decipher'),
+const btncipher=()=>{ 
+    document.getElementById("resultado").value=cipher.encode(offset.value,string.value);
+}
+document.getElementById("btncipher").addEventListener("click",btncipher);
 
-input = document.getElementById('inputText'),
-show = document.getElementById('show'),
-number = document.getElementById('key');
-
+const btndecipher=()=>{ 
+    document.getElementById("resultado").value=cipher.decode(offset.value,string.value);
+}
+document.getElementById("btndecipher").addEventListener("click",btndecipher);
