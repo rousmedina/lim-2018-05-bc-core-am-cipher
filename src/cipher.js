3e418516b0370eb5a8a57d64c5a7474cecf0f60c
rousmedina <rousmedina21@gmail.com>
2018-06-01 12:05:10 -0500

    subiendo test

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,14 @@
 window.cipher = {
   encode:(offset,string) => {
     let complet = '';
-    for (i = 0; i < string.length; i++) {
+    for (let i = 0; i < string.length; i++) {
       let toAscii = string.charCodeAt(i);
       if (toAscii >= 65 && toAscii <= 90) {
         complet += String.fromCharCode((toAscii - 65 + parseInt(offset)) % 26 + 65); // Mayus
       } else if (toAscii >= 97 && toAscii <= 122) {
         complet += String.fromCharCode((toAscii - 97 + parseInt(offset)) % 26 + 97); // Minus
       } else if (toAscii == 32) {
-        complet += "";
+        complet += " ";
       }
     }        
     return complet;    
@@ -16,16 +16,17 @@ window.cipher = {
 
   decode:(offset,string) => {
     let complet = '';
-    for (i = 0; i < string.length; i++) {
+    for (let i = 0; i < string.length; i++) {
       let toAscii = string.charCodeAt(i);
       if (toAscii >= 65 && toAscii <= 90) {
         complet += String.fromCharCode((toAscii + 65 - parseInt(offset)) % 26 + 65); // Mayus
       } else if (toAscii >= 97 && toAscii <= 122) {
         complet += String.fromCharCode((toAscii + 97 - parseInt(offset)-12) % 26 + 97); // Minus
       } else if (toAscii == 32) {
-        complet += "";
+        complet += " ";
       }
     }        
     return complet;    
   }
+ // createCipherWithOffset
 }
