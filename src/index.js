8bdc78641d44ba93bd20e67c43696e884cc205ac
rousmedina <rousmedina21@gmail.com>
2018-06-04 08:41:30 -0500

    subiendo archivos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const offset = document.getElementById('key');
 const string = document.getElementById("inputText")
-
+/* se llama al button-cipher cuando el mouse haga click y devuelve la función */
 const btncipher=()=>{ 
     document.getElementById("resultado").value=cipher.encode(offset.value,string.value);
 }
